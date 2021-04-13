1d9e13c0d9e22c2df24aea3a7d04596d62442ea3
nokonoko1203 <healtheworld.1203@gmail.com>
2021-04-13 16:52:50 +0900

    ファイルの読み込み先、書き出し先を変更

--- scripts/02_merge_shp_to_gpkg.py
+++ scripts/02_merge_shp_to_gpkg.py
@@ -4,17 +4,16 @@ from pathlib import Path
 import geopandas as gpd
 import pandas as pd
 
-zip_path_list = [Path(l) for l in glob.glob("./*.zip")]
+zip_path_list = [Path(l) for l in glob.glob("../download/*.zip")]
 
-town_gdf = pd.concat([
-    gpd.read_file("zip://" + str(zipfile))
-    for zipfile in zip_path_list
-]).pipe(gpd.GeoDataFrame)
+town_gdf = pd.concat(
+    [gpd.read_file("zip://" + str(zipfile)) for zipfile in zip_path_list]
+).pipe(gpd.GeoDataFrame)
 
-town_gdf["AREA_CODE"] = town_gdf['PREF'].str.cat(town_gdf['CITY'])
+town_gdf["AREA_CODE"] = town_gdf["PREF"].str.cat(town_gdf["CITY"])
 city_gdf = town_gdf.dissolve(by="AREA_CODE", as_index=False)
 pref_gdf = city_gdf.dissolve(by="PREF", as_index=False)
 
-pref_gdf.to_file("boundary.gpkg", layer='pref', driver="GPKG")
-city_gdf.to_file("boundary.gpkg", layer='city', driver="GPKG")
-town_gdf.to_file("boundary.gpkg", layer='town', driver="GPKG")
+pref_gdf.to_file("../data/boundary.gpkg", layer="pref", driver="GPKG")
+city_gdf.to_file("../data/boundary.gpkg", layer="city", driver="GPKG")
+town_gdf.to_file("../data/boundary.gpkg", layer="town", driver="GPKG")
