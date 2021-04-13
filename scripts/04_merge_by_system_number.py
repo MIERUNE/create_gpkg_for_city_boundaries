41e796c67dfd55df738b11be73e472ba48dce549
nokonoko1203 <healtheworld.1203@gmail.com>
2021-04-13 17:06:34 +0900

    ファイルの読み込み先、書き出し先を変更

--- scripts/04_merge_by_system_number.py
+++ scripts/04_merge_by_system_number.py
@@ -1,13 +1,13 @@
 import geopandas as gpd
 
-merge_boundary_gdf = gpd.read_file("merge_boundary.gpkg", layer='add_system_number')
+merge_boundary_gdf = gpd.read_file(
+    "../data/merge_boundary.gpkg", layer="add_system_number"
+)
 
 plane_rectangular_gdf = merge_boundary_gdf.dissolve(by="system_number", as_index=False)
-necessary_columns = [
-    "system_number",
-    "geometry"
-]
+necessary_columns = ["system_number", "geometry"]
 plane_rectangular_gdf = plane_rectangular_gdf[necessary_columns]
 
-plane_rectangular_gdf.to_file("merge_boundary.gpkg", layer="plane_rectangular", driver="GPKG")
-
+plane_rectangular_gdf.to_file(
+    "../data/merge_boundary.gpkg", layer="plane_rectangular", driver="GPKG"
+)
